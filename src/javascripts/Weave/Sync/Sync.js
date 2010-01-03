7d59fddad3eb0ec0cade980c41472ce276bc6f59
Leslie Michael Orchard <l.m.orchard@pobox.com>
2010-01-02 22:43:40 -0500

    Reworked sync objects to be per-collection

--- src/javascripts/Weave/Sync/Sync.js
+++ src/javascripts/Weave/Sync/Sync.js
@@ -5,7 +5,15 @@
  */
 /*jslint laxbreak: true */
 /*global Mojo, Weave, Chain, Class, Ajax */
-Weave.Sync = Class.create(/** @lends Weave.Sync */ {
+
+/** @namespace */
+Weave.Sync = {};
+
+Weave.Sync.BasicSync = Class.create(/** @lends Weave.Sync.BasicSync */ {
+
+    /** Local storage silo class */
+    silo_class: Weave.Storage.HistorySilo,
+    collection_name: 'history',
 
     /**
      * Weave sync engine
@@ -13,30 +21,53 @@ Weave.Sync = Class.create(/** @lends Weave.Sync */ {
      * @author l.m.orchard@pobox.com
      */
     initialize: function (options) {
-
         this.options = Object.extend({
             "service": null
         }, options || {});
 
-        this.silos = {
-            history: new Weave.Storage.HistorySilo(),
-            bookmarks: new Weave.Storage.BookmarkSilo(),
-            tabs: new Weave.Storage.TabSilo()
-        };
-
+        this.service = this.options.service;
+        this.collection = this.service[this.collection_name];
+        this.silo = new this.silo_class();
     },
 
     open: function (on_success, on_failure) {
-        var chain = new Decafbad.Chain([], this, on_failure);
-        
-        $H(this.silos).each(function (pair) {
-            chain.push(function (c) {
-                pair.value.open(c.nextCb(), on_failure);
-            });
-        }, this);
-
-        chain.push(on_success).start();
+        var chain = new Decafbad.Chain([
+            function (chain) {
+                this.silo.open(chain.nextCb(), on_failure);
+            },
+            on_success
+        ], this, on_failure).start();
     },
 
     EOF:null // I hate trailing comma errors
+});
+
+/**
+ * @class
+ * @augments Weave.Sync.BasicSync
+ */
+Weave.Sync.HistorySync = Class.create(Weave.Sync.BasicSync, /** @lends Weave.Sync.HistorySync */ {
+    silo_class: Weave.Storage.HistorySilo,
+    collection_name: 'history',
+    EOF: null
+});
+
+/**
+ * @class
+ * @augments Weave.Sync.BasicSync
+ */
+Weave.Sync.BookmarkSync = Class.create(Weave.Sync.BasicSync, /** @lends Weave.Sync.BookmarkSync */ {
+    silo_class: Weave.Storage.BookmarkSilo,
+    collection_name: 'bookmarks',
+    EOF: null
+});
+
+/**
+ * @class
+ * @augments Weave.Sync.BasicSync
+ */
+Weave.Sync.TabSync = Class.create(Weave.Sync.BasicSync, /** @lends Weave.Sync.TabSync */ {
+    silo_class: Weave.Storage.TabSilo,
+    collection_name: 'tabs',
+    EOF: null
 });
